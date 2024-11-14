58c5d680f648671f1a4e156de53589dd9a2f2653
RemasNg1 <remas-2290@hotmail.com>
2024-11-14 11:56:56 +0300

    Add project files.

---
+++ SatrAspProject/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SatrAspProject.Data;
+
+namespace SatrAspProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductsController(AppDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            var product = _db.Products.ToList();
+            ViewData["product"]=product;
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var product = _db.Products.ToList().Find(p => p.Id==id);
+            if(product==null || id==null)
+            {
+                return View("NotFound");
+            }
+            ViewData["product"]=product;
+            return View();
+        }
+
+        public IActionResult Random()
+        {
+            var products = _db.Products.ToList();
+            var randomProduct = products[new Random().Next(products.Count)];
+            ViewData["random"]=randomProduct;
+            return View();
+        }
+
+    }
+}
---
+++ SatrAspProject/Migrations/20241113121414_SecondMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SatrAspProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Phone", 666.2f },
+                    { 2, "Laptop", 100.2f },
+                    { 3, "TV", 345.2f }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
