d726b7d1b5f93f2898c0ff2f0b35ddf0cc18a670
RemasNg1 <remas-2290@hotmail.com>
2024-11-17 10:43:15 +0300

    Updated

--- SatrAspProject/Data/AppDbContext.cs
+++ SatrAspProject/Data/AppDbContext.cs
@@ -10,15 +10,7 @@ namespace SatrAspProject.Data
         public DbSet<ProductModel> Products { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 1, Name="Phone",  Price=666.2f });
-            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 2, Name="Laptop", Price=100.2f });
-            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 3, Name="TV", Price=345.2f });
-
-
-
-        }
+       
 
     }
 
